c5655c830172a1b6b03376f9e60fa1e43d7ee442
Anastasiia Vashchenko <62261242+ana-stay-sha@users.noreply.github.com>
2021-06-09 19:03:55 +0300

    add change

--- script/script.js
+++ script/script.js
@@ -1,122 +1,135 @@
+"use strict";
 
 const form = document.querySelector('.formWithValidation');
-const validateBtn = form.querySelector('.validateBtn');
-let fields = form.querySelectorAll('.field');
+const validateBtn = form.querySelector('.form__submit-btn');
+const fields = form.querySelectorAll('.contact-us__input');
+const fullName = document.getElementById('#fullname');
+const errorMessage = document.querySelectorAll('.error-message');
+const checkboxe = document.querySelector('.custom-checkbox');
 
 
-let validateEmail = function() {
+const generateError = function (text) {
+    errorMessage.data = text;
+    return errorMessage
+  }
+
+  const checkFieldsPresence = function () {
+    for (let i = 0; i < fields.length; i++) {
+      if (!fields[i].value) {
+        const error = generateError('Cant be blank');
+        errorMessage[i].innerHTML = error.data;
+        fields[i].style.borderColor = 'red';
+      }
+        else{
+        errorMessage[i].innerHTML = '';
+        fields[i].style.borderColor = '#999999';
+      }
+    }
+
+
+  }
+
+const validateName = function (){
+    const regName = /^[A-z\u00C0-\u00ff\.,-\/#!$%\^&\*;:{}=\-_`~()]+$/;
+    let fullName = document.querySelector('#fullname')
+    fullName = fullName.value;
+        if (fullName.length > 0 && fullName.length <= 2){
+            const error = generateError('Username must be atleast 2 charactrs long, Please Try Again')
+            errorMessage[0].innerHTML = error.data;
+        } 
+        else if (fullName.length > 0 && !regName.test(fullName)){
+            const error = generateError('Use only Latin characters, Please Try Again')
+            errorMessage[0].innerHTML = error.data;
+        }
+} 
+
+const validateEmail = function() {
     let email = document.querySelector('#email');
     email = email.value;
-    let reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    if (!reg.test(email)){
-        let error = generateError('Invalid email, Please Try Again')
-        form[1].parentElement.insertBefore(error, fields[1]);
+    const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.length != 0 && !reg.test(email)){
+        const error = generateError('Invalid email, Please Try Again');
+        errorMessage[1].innerHTML = error.data;
     } 
 }
 
-let removeValidation = function () {
-    let errors = form.querySelectorAll('.error')
-  
-    for (let i = 0; i < errors.length; i++) {
-      errors[i].remove()
+const checkCheckBoxes = function(){
+    if (checkboxe.checked == false){
+        const error = generateError('You must agree to the terms first');
+        errorMessage[4].innerHTML = error.data;
     }
-  }
-
-let generateError = function (text) {
-    let error = document.createElement('div')
-    error.className = 'error'
-    error.style.color = 'red'
-    error.innerHTML = text
-    return error
-  }
-
-  let checkFieldsPresence = function () {
-    for (let i = 0; i < fields.length; i++) {
-      if (!fields[i].value) {
-        console.log('field is blank', fields[i])
-        let error = generateError('Cant be blank')
-        form[i].parentElement.insertBefore(error, fields[i])
+    else{
+        errorMessage[4].innerHTML = '';
       }
+}
+
+const serialize = function(data){
+    let obj = {};
+    for (let [key, value] of data) {
+        if (obj[key] !== undefined) {
+            if (!Array.isArray(obj[key])) {
+                obj[key] = [obj[key]];
+            }
+            obj[key].push(value);
+        } else {
+            obj[key] = value;
+        }
     }
-  }
+    return obj;
+}
 
-let validate = function (){
-    let fullName = document.querySelector('#fname')
-    fullName = fullName.value;
-        if (fullName.length > 0 && fullName.length <= 2){
-            let error = generateError('Username must be atleast 2 charactrs long, Please Try Again')
-            form[0].parentElement.insertBefore(error, fields[0]);
-        } 
+const sendRequest = function(method, url, body = null){
+    const headers = {
+        'Content-Type' : 'application/json'
+    }
+    return fetch (url, {
+        method: method,
+        body: JSON.stringify(body),
+        headers : headers
+    }).then( response => {
+        if (response.ok){
+            return response.json() 
+        }
 
-   } 
+        return response.json().then(error => {
+            const e = new Error('Something went wrong')
+            e.data = error
+            throw e
+        })
+    })
+}
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    removeValidation();
-
     checkFieldsPresence()
 
-    validate();
-    
+    validateName();
+
     validateEmail();
+
+    checkCheckBoxes();
+
+    const data = new FormData(form);
+    const body = serialize(data);
+
+    sendRequest('POST', requestUrl, body)
+        .then(data => {
+            let textValue = 'You are sending data: \n';
+            for (let property in data) {
+                if (data[property] == ''){
+                    return false
+                }
+                else{
+                    textValue += `${property}: ${data[property]} \n`
+                }
+                
+            }
+            confirm(textValue)
+            form.reset();
+        })
+        .catch(err => console.log(err))
+
   })
 
-// fetch('#')
-//   .then(data => {
-//       console.log(data)
-//   })
-
-  /*
-Создание XMLHttpRequest-объекта
-Возвращает созданный объект или null, если XMLHttpRequest не поддерживается
-*/
-function createRequestObject() {
-    var request = null;
-    try {
-        request=new ActiveXObject('Msxml2.XMLHTTP');
-    } catch (e){}
-    if(!request) try {
-        request=new ActiveXObject('Microsoft.XMLHTTP');
-    } catch (e){}
-    if(!request) try {
-        request=new XMLHttpRequest();
-    } catch (e){}
-    return request;
-}
-
-/*
-Кодирование данных (простого ассоциативного массива вида { name : value, ...} в
-URL-escaped строку (кодировка UTF-8)
-*/
-function urlEncodeData(data) {
-    var query = [];
-    if (data instanceof Object) {
-        for (var k in data) {
-            query.push(encodeURIComponent(k) + "=" + encodeURIComponent(data[k]));
-        }
-        return query.join('&');
-    } else {
-        return encodeURIComponent(data);
-    }
-}
-
-/*
-Выполнение POST-запроса 
-url  - адрес запроса
-data - параметры в виде простого ассоциативного массива { name : value, ...} 
-callback - (не обяз.) callback-функция, которая будет вызвана после выполнения запроса и получения ответа от сервера
-*/
-function serverRequest(url, data, callback) {
-    var request = createRequestObject();
-    if(!request) return false;
-    request.onreadystatechange  = function() { 
-            if(request.readyState == 4 && callback) callback(request);
-        };
-    request.open('POST', url, true);
-    if (request.setRequestHeader)
-        request.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-    request.send(urlEncodeData(data));
-    return true;
-}
+const requestUrl = 'https://jsonplaceholder.typicode.com/users';
